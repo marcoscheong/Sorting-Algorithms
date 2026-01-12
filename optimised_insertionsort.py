ad9939faec84ba070476fb8cd9a259dd4c0fdbcd
marcoscheong <cheong.hao.ting.marcos_2025@nyjc.edu.sg>
2026-01-12 15:20:22 +0800

    Solved an error

--- optimised_insertionsort.py
+++ optimised_insertionsort.py
@@ -1,8 +1,8 @@
 def insertion_sort(lst):
-    for i in range(2, len(lst)):
+    for i in range(1, len(lst)):
         target = lst[i]
         j = i - 1
-        while j > 0 and lst[j] > target:
+        while j >= 0 and lst[j] > target:
             lst[j + 1] = lst[j]
             j = j - 1
         lst[j + 1] = target
@@ -10,9 +10,8 @@ def insertion_sort(lst):
     return lst
     
 
-int_list = [1,2323,323,2132332432432,4324,23432432432432,4324,32432,42432432432,234233423422343]
+int_list = [3,2,1]
 print(insertion_sort(int_list))
-
 # PROCEDURE InsertionSort(BYREF Array : ARRAY)
 #     // Sort an array of integers
 #     DECLARE i : INTEGER          // Unsorted loop index
